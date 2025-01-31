bb41fd180768cc59240b45bfd8f4e65d1a9c48fd
Youssef <youssefaboelenin79@gmail.com>
2025-01-31 17:49:04 +0200

    Finish forget password UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app_g2/forget_password_screen.dart';
 import 'package:movies_app_g2/home_screen.dart';
 
 void main() {
@@ -13,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName,
+      initialRoute: ForgetPasswordScreen.routeName,
       routes: {
-        HomeScreen.routeName: (context) => HomeScreen(),
+        ForgetPasswordScreen.routeName: (context) => ForgetPasswordScreen(),
       },
       themeMode: ThemeMode.dark,
     );
